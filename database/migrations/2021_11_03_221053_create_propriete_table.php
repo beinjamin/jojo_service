a22400d7e751010e453f91fe11c7b1b3565414ed
beinjamin <jauresbeinjamin@gmail.com>
2021-11-03 23:12:50 +0100

    Creation de la migration propriete

---
+++ database/migrations/2021_11_03_221053_create_propriete_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProprieteTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('propriete', function (Blueprint $table) {
+            $table->id();
+            $table->string("nom");
+            $table->boolean("estObligatoir")->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('propriete');
+    }
+}

5b9d343924572215d7025085efa20c49c322d4a4
beinjamin <jauresbeinjamin@gmail.com>
2021-11-04 00:31:22 +0100

    Mise a jour de la migration duree locations

--- database/migrations/2021_11_03_232854_create_duree_locations_table.php
+++ database/migrations/2021_11_03_232854_create_duree_locations_table.php
@@ -15,7 +15,8 @@ class CreateDureeLocationsTable extends Migration
     {
         Schema::create('duree_locations', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->string("libelle");
+            $table->time("valeurEnHeure");
         });
     }
 

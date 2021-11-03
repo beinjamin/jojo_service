ece2bbb7f585b5120754d7f9783def558260d04b
beinjamin <jauresbeinjamin@gmail.com>
2021-11-04 00:23:48 +0100

    Creation de la migration client

---
+++ database/migrations/2021_11_03_231626_create_clients_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateClientsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clients', function (Blueprint $table) {
+            $table->id();
+            $table->string("nom");
+            $table->string("prenom");
+            $table->date("dateNaissance");
+            $table->string("lieuNaissance");
+            $table->char("sex");
+            $table->string("ville");
+            $table->string("nationalié");
+            $table->date("pays");
+            $table->string("adresse");
+            $table->string("téléphone1");
+            $table->string("téléphone2");
+            $table->string("pieceIdentité");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('clients');
+    }
+}

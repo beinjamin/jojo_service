4e3f7eb325ead4e2016eb9dbe97858afd461dbaf
beinjamin <jauresbeinjamin@gmail.com>
2021-11-03 22:07:02 +0100

    Initialisation des migrations

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,7 +15,11 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('nom');
+            $table->string('prenom');
+            $table->char('sexe');
+            $table->string('telephone1');
+            $table->string('telephone2')->nullable;
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');

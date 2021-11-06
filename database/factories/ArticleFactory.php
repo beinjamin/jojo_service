363c39c516a47069e0426a9717bcb9befaacce0e
beinjamin <jauresbeinjamin@gmail.com>
2021-11-06 21:11:04 +0100

    Initialisation de notre ArticleFactory via notre model Article

--- database/factories/ArticleFactory.php
+++ database/factories/ArticleFactory.php
@@ -4,17 +4,23 @@ namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 
+
 class ArticleFactory extends Factory
 {
+    protected $model = Article::class;
     /**
      * Define the model's default state.
+     *
      *
      * @return array
      */
     public function definition()
     {
         return [
-            //
+            "nom" => $this->fake->lastName,
+            "noSerie" => $this->fake->swiftBicNumber,
+            "imageUrl" => $this->faker->imageUrl(),
+            "estDisponible" => rand(0, 1),
         ];
     }
 }

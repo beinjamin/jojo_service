0a9ed91d9695b51295e606ef9443f2f44793a20f
beinjamin <jauresbeinjamin@gmail.com>
2021-11-08 11:16:28 +0100

    Resolution du bug sur le seed

--- database/factories/ArticleFactory.php
+++ database/factories/ArticleFactory.php
@@ -18,8 +18,8 @@ class ArticleFactory extends Factory
     public function definition()
     {
         return [
-            "nom" => $this->fake->lastName,
-            "noSerie" => $this->fake->swiftBicNumber,
+            "nom" => $this->faker->lastName,
+            "noSerie" => $this->faker->swiftBicNumber,
             "imageUrl" => $this->faker->imageUrl(),
             "type_article_id" => rand(1, 4),
             "estDisponible" => rand(0, 1),

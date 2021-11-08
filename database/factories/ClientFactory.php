02d166d0b3306380682ef0c80af32eff928f59db
beinjamin <jauresbeinjamin@gmail.com>
2021-11-08 11:50:36 +0100

    Mise A jour de client factory

--- database/factories/ClientFactory.php
+++ database/factories/ClientFactory.php
@@ -15,8 +15,23 @@ class ClientFactory extends Factory
      */
     public function definition()
     {
+        $ville = $this->faker->city;
+        $pays = $this->faker->country;
+
         return [
-            //
+            "nom" => $this->faker->lastName,
+            "prenom" => $this->faker->firstName,
+            "sexe" => array_rand(["F", "H"], 1),
+            "dateNaissance" => $this->faker->dateTimeBetween("1960-01-01", "2004-12-30"),
+            "lieuNaissance" =>  "$pays, $ville",
+            "nationalite" => $this->faker->country,
+            "pays" => $pays,
+            "ville" => $ville,
+            "adresse" => $this->faker->address,
+            "telephone1" => $this->faker->phoneNumber,
+            "telephone2" => $this->faker->phoneNumber,
+            "pieceIdentite" => array_rand(["CNI", "PASSPORT", "PERMIS DE CONDUIRE"], 1),
+            "noPieceIdentite" => $this->faker->creditCardNumber,
         ];
     }
 }

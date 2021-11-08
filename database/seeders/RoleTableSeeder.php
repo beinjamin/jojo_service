8483192999effcd9737dd99da000e4f8b7459bd5
beinjamin <jauresbeinjamin@gmail.com>
2021-11-08 12:19:30 +0100

    Insertion des roles dans la table role

--- database/seeders/RoleTableSeeder.php
+++ database/seeders/RoleTableSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class RoleTableSeeder extends Seeder
 {
@@ -13,6 +14,11 @@ class RoleTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table("roles")->insert([
+            ["nom" => "superadmin"],
+            ["nom" => "admin"],
+            ["nom" => "manager"],
+            ["nom" => "employe"]
+        ]);
     }
 }

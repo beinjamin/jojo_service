0bb35c7fbef31d9530d5e23b659059c72964e41f
beinjamin <jauresbeinjamin@gmail.com>
2021-11-08 11:02:05 +0100

    Update

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+
         $this->call(TypeArticleTableSeeder::class);
         Article::factory(10)->create();
     }

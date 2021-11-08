9265061c497c3e0b899cd70e2f6078a8a8ba4e8e
beinjamin <jauresbeinjamin@gmail.com>
2021-11-08 11:44:14 +0100

    Mise A jour de client factory

--- database/factories/ClientFactory.php
+++ database/factories/ClientFactory.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 
 class ClientFactory extends Factory
 {
+
+    protected $model = Client::class;
     /**
      * Define the model's default state.
      *

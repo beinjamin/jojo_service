2d5341b98010d2905bbb73bff6d4ffcbc665a2b7
beinjamin <jauresbeinjamin@gmail.com>
2021-11-13 19:26:50 +0100

    Mise des relation

--- app/Models/Location.php
+++ app/Models/Location.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Location extends Model
 {
     use HasFactory;
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+    public function client()
+    {
+        return $this->belongsTo(Client::class);
+    }
+    public function statut()
+    {
+        return $this->belongsTo(Client::class);
+    }
 }

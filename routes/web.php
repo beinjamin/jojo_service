647db5fc96b0d9b888d78154c186af1650cf6858
beinjamin <jauresbeinjamin@gmail.com>
2021-11-13 18:18:17 +0100

    Test des relation sur la route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Article;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+Route::get('/articles', function () {
+    return Article::paginate(5);
 });

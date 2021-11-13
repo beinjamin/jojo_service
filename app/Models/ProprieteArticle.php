77d95d7cf505eca8f0d05ec8198cfd201cbc7e89
beinjamin <jauresbeinjamin@gmail.com>
2021-11-13 19:20:50 +0100

    Mie en relation propritéarticle et typearticle

--- app/Models/ProprieteArticle.php
+++ app/Models/ProprieteArticle.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class ProprieteArticle extends Model
 {
     use HasFactory;
+    public function type()
+    {
+        return $this->belongsTo(TypeArticle::class, "type_article_id", "id");
+    }
 }

b2b4e923c16ee00e9696ce3bb29887eae7a5d491
beinjamin <jauresbeinjamin@gmail.com>
2021-11-13 19:27:45 +0100

    Creation du model status

---
+++ app/Models/StatutLocation.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class StatutLocation extends Model
+{
+    use HasFactory;
+}

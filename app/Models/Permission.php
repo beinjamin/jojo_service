80c0dfd708ffd0ed78c244b27b7e14fc0d4ce277
beinjamin <jauresbeinjamin@gmail.com>
2021-11-14 09:18:22 +0100

    Mise en relation permission et user

--- app/Models/Permission.php
+++ app/Models/Permission.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Permission extends Model
 {
     use HasFactory;
+    public function users()
+    {
+        return $this->belongsToMany(User::class, "user_permission",  "permission_id", "user_id");
+    }
 }

7871afececb3c9c9a2e6cdc8fb06549f0227c0ed
beinjamin <jauresbeinjamin@gmail.com>
2021-11-14 22:49:41 +0100

    Reogardisation de la view et creaction du components

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,27 +1,10 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Management Service | Starter</title>
-
-    <link rel="stylesheet" href="{{mix("css/app.css")}}" />
-
-    @livewireStyles
-
-</head>
-<body class="hold-transition sidebar-mini">
-<div class="wrapper">
-
-  <!-- Navbar -->
-
-  <!-- /.navbar -->
+<  <!-- /.navbar -->
 
   <!-- Main Sidebar Container -->
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="index3.html" class="brand-link">
-      <span class="brand-text font-weight-bold" style="font-size: 1.3em;"><b>Jaures Beinjamin</b></span>
+      <span class="brand-text font-weight-bold" style="font-size: 1.3em;"><b>YBLTD</b></span>
     </a>
 
     <!-- Sidebar -->
@@ -30,65 +13,6 @@
       <div class="user-panel mt-3 pb-3 mb-3 d-flex">
         <div class="image">
               <img src="{{asset('images/user1-128x128.jpg')}}" alt="User Avatar" class="img-size-50 mr-3 img-circle">
-
-
-<div class="card card-primary card-outline">
-              <div class="card-body box-profile">
-                <div class="text-center">
-                  <img class="profile-user-img img-fluid img-circle" src="../../dist/img/user4-128x128.jpg" alt="User profile picture">
-                </div>
-
-                <h3 class="profile-username text-center">Nina Mcintire</h3>
-
-                <p class="text-muted text-center">Software Engineer</p>
-
-                <ul class="list-group list-group-unbordered mb-3">
-                  <li class="list-group-item">
-                    <b>Followers</b> <a class="float-right">1,322</a>
-                  </li>
-                  <li class="list-group-item">
-                    <b>Following</b> <a class="float-right">543</a>
-                  </li>
-                  <li class="list-group-item">
-                    <b>Friends</b> <a class="float-right">13,287</a>
-                  </li>
-                </ul>
-
-                <a href="#" class="btn btn-primary btn-block"><b>Follow</b></a>
-              </div>
-              <!-- /.card-body -->
-            </div>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
         </div>
         <div class="info">
           <a href="#" class="d-block"></a>
@@ -98,7 +22,7 @@
 
 
       <!-- Sidebar Menu -->
-
+      <x-menu />
       <!-- /.sidebar-menu -->
     </div>
     <!-- /.sidebar -->
@@ -113,7 +37,7 @@
     <!-- Main content -->
     <div class="content">
       <div class="container-fluid">
-
+        @yield("contenu")
         <!-- /.row -->
       </div><!-- /.container-fluid -->
     </div>
@@ -122,7 +46,7 @@
   <!-- /.content-wrapper -->
 
   <!-- Control Sidebar -->
-
+  <x-sidebar />
   <!-- /.control-sidebar -->
 
   <!-- Main Footer -->
@@ -132,7 +56,7 @@
       Anything you want
     </div>
     <!-- Default to the left -->
-    <strong>Copyright &copy; 2021 <a href="">Jaures.com</a>.</strong> All rights reserved.
+    <strong>Copyright &copy; 2021 <a href="https://adminlte.io">Codeurspassionnes.com</a>.</strong> All rights reserved.
   </footer>
 </div>
 <!-- ./wrapper -->

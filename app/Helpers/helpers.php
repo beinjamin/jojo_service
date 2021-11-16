7964b3241510d2b0599474806d4fbf5c4c1141c1
beinjamin <jauresbeinjamin@gmail.com>
2021-11-16 23:16:06 +0100

    Creation d'une function helpers  pour afficher le profil de l'utilisateur sur la topnav

--- app/Helpers/helpers.php
+++ app/Helpers/helpers.php
@@ -5,3 +5,17 @@ function userFullName()
 {
     return auth()->user()->prenom . "" . auth()->user()->nom;
 }
+
+function getRolesName()
+{
+    $rolesName = "";
+    foreach (auth()->user()->roles as $role) {
+        $rolesName += $role->nom;
+        if ($i < sizeof(auth()->user()->roles) - 1) {
+            $rolesName .= ",";
+        }
+
+        $i++;
+    }
+    return   $rolesName;
+}

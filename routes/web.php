13cea86ffd71296b95dc401a8df3175078c7fa2a
beinjamin <jauresbeinjamin@gmail.com>
2021-11-16 23:40:07 +0100

    Update de la view

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,4 @@ use Illuminate\Support\Facades\Route;
 Auth::routes();
 
 Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/habilitations/utilisateurs', [App\Http\Controllers\HomeController::class, 'index'])->name('utilisateurs');

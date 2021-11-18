295a50e043b551a315a6b68ab69447fb92404ce0
beinjamin <jauresbeinjamin@gmail.com>
2021-11-18 19:03:14 +0100

    Verification de l'utilisateur auth

--- app/Http/Middleware/AdminMiddleware.php
+++ app/Http/Middleware/AdminMiddleware.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Gate;
 
 class AdminMiddleware
 {
@@ -16,6 +17,7 @@ class AdminMiddleware
      */
     public function handle(Request $request, Closure $next)
     {
-        return $next($request);
+        if (Gate)
+            return $next($request);
     }
 }

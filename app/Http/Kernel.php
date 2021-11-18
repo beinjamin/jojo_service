caa08d4bbeb72bbecae79303fa5d1b1e3da797d5
beinjamin <jauresbeinjamin@gmail.com>
2021-11-18 19:09:56 +0100

    Mise en place de la middelware dans le kernel

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -56,6 +56,9 @@ class Kernel extends HttpKernel
     protected $routeMiddleware = [
         'auth' => \App\Http\Middleware\Authenticate::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
+
+        'auth.admin' =>  \App\Http\Middleware\AdminMiddleware::class,
+
         'cache.headers' => \Illuminate\Http\Middleware\SetCacheHeaders::class,
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,

28bcc2e927f4c8984b30ea970e97a1a2ae85b907
beinjamin <jauresbeinjamin@gmail.com>
2021-11-18 19:45:09 +0100

    Mise a jour du middleware manager et superadmin

--- app/Http/Middleware/ManagerMiddleware.php
+++ app/Http/Middleware/ManagerMiddleware.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Gate;
 
 class ManagerMiddleware
 {
@@ -16,6 +17,9 @@ class ManagerMiddleware
      */
     public function handle(Request $request, Closure $next)
     {
-        return $next($request);
+        if (Gate::allows("manager")) {
+            return $next($request);
+        }
+        return redirect()->route("home");
     }
 }
--- app/Http/Middleware/SuperAdminMiddleware.php
+++ app/Http/Middleware/SuperAdminMiddleware.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Gate;
 
 class SuperAdminMiddleware
 {
@@ -16,6 +17,10 @@ class SuperAdminMiddleware
      */
     public function handle(Request $request, Closure $next)
     {
-        return $next($request);
+
+        if (Gate::allows("superadmin")) {
+            return $next($request);
+        }
+        return redirect()->route("home");
     }
 }

656fe6d1ce78056a4c033a7a8eea569a795c14c0
beinjamin <jauresbeinjamin@gmail.com>
2021-11-20 01:23:12 +0100

    Update menu style

--- resources/views/components/menu.blade.php
+++ resources/views/components/menu.blade.php
@@ -40,7 +40,7 @@
         @can("admin")
 
 
-        <li class="nav-item ">
+        <li class="nav-item menu-open">
              <a href="" class="nav-link active" >
               <i class=" nav-icon fas fa-user-shield"></i>
               <p>

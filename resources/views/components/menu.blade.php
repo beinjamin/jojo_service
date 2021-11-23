6f1a2f130363c94cbc1f9a2834d0723dd1c54682
beinjamin <jauresbeinjamin@gmail.com>
2021-11-23 21:44:54 +0100

    Appel de la funtion dans le menu blade pour gere la class Accueil

--- resources/views/components/menu.blade.php
+++ resources/views/components/menu.blade.php
@@ -3,7 +3,7 @@
           <!-- Add icons to the links using the .nav-icon class
                with font-awesome or any other icon font library -->
         <li class="nav-item">
-            <a href="{{ route('home') }}" class="nav-link {">
+            <a href="{{ route('home') }}" class="nav-link{{ setMenuActive('home') }}">
               <i class="nav-icon fas fa-home"></i>
               <p>
                 Accueil

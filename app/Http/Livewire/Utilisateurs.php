a0e924a7e1ea4b2a7e4e6c2afc0e01ed92f6f851
beinjamin <jauresbeinjamin@gmail.com>
2021-11-26 22:05:32 +0100

    Recuperation de tout les users pour affichers dans utilisateur.index

--- app/Http/Livewire/Utilisateurs.php
+++ app/Http/Livewire/Utilisateurs.php
@@ -2,13 +2,16 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\User;
 use Livewire\Component;
 
 class Utilisateurs extends Component
 {
     public function render()
     {
-        return view('livewire.utilisateurs.index')
+        return view('livewire.utilisateurs.index', [
+            "users" => User::all()
+        ])
             ->extends("layouts.master")
             ->section("contenu");
     }

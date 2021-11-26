883dd6a24c3cb5f7e4944dfd29eef9dab73beca2
beinjamin <jauresbeinjamin@gmail.com>
2021-11-26 21:58:42 +0100

    Update

--- app/Http/Livewire/Utilisateurs.php
+++ app/Http/Livewire/Utilisateurs.php
@@ -8,6 +8,8 @@ class Utilisateurs extends Component
 {
     public function render()
     {
-        return view('livewire.utilisateurs.index');
+        return view('livewire.utilisateurs.index')
+            ->extends("layouts.master")
+            ->section("contenu");
     }
 }

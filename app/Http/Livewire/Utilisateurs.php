9cf220242f7d452ec2fbdfdec02ad12057f6936c
beinjamin <jauresbeinjamin@gmail.com>
2021-12-02 19:43:20 +0100

    Gestion des pagination

--- app/Http/Livewire/Utilisateurs.php
+++ app/Http/Livewire/Utilisateurs.php
@@ -10,7 +10,7 @@ class Utilisateurs extends Component
     public function render()
     {
         return view('livewire.utilisateurs.index', [
-            "users" => User::all()
+            "users" => User::paginate(5)
         ])
             ->extends("layouts.master")
             ->section("contenu");
